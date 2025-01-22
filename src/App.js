6481205101ef1b14b63a0aea0f42b93a47122ca8
Pratik <pgumthanavar05@gmail.com>
2025-01-22 13:19:44 +0530

    Added routes and created the Components with seperate paths for them

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Header from "./components/Header.js";
-import Body from "./components/Body.js";
-
+import Header from './components/Header.js';
+import Body from './components/Body.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Error from './components/Error.js';
 
 const AppLayout = () => {
-    return (
-        <div>
-            <Header />
-            <Body />
-        </div>
-    );
-}
-const root = ReactDOM.createRoot(document.getElementById("root"));
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-root.render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact us for more info</h1>
+    </div>
+  );
+};
+
+export default Contact;
